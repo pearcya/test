37517b6a1b0ab81b8727c614acaef2e4890423db
Andrew Pearcy <andrew.pearcy2121@hotmail.com>
2018-07-04 23:46:39 -0400

    Initial commit

---
+++ app/src/main/java/com/techcetro/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.techcetro.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+
+    Button bt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bt = (Button) findViewById()
+    }
+}
